395ca3a987716a233705da7b97df6ab9f88ff98e
VP <vaclav.pazderka@logex.com>
2026-01-12 22:26:10 +0100

    example: simple insert query

---
+++ polymarket/src/examples/run_simple_query_db_insert.py
@@ -0,0 +1,24 @@
+import asyncio
+from polymarket.src.stream.binance import binance_price_ws
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from polymarket.src.db.ingest_stream import insert_into_db, parse_message
+
+conf = (
+    'http::addr=localhost:9000;'
+    'username=admin;password=quest;'
+    'auto_flush_rows=100;auto_flush_interval=1000;')
+table_name = "trade_stream"
+symbols = ["btcusdt"]
+queue = asyncio.Queue()
+
+
+async def main(conf, queue, symbols, table_name):
+    executor = ProcessPoolExecutor(max_workers=10)
+    db_task = asyncio.create_task(insert_into_db(conf, table_name, queue, parse_message, executor))
+    await binance_price_ws(queue=queue, symbols=symbols)
+    await queue.put(None)
+    await db_task
+
+asyncio.run(
+    main(conf, queue, symbols, table_name)
+)
