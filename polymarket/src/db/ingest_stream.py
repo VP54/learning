ced3f9f56f30d3be657e6e33ed0a44844824a72b
VP <vaclav.pazderka@logex.com>
2026-01-12 22:23:07 +0100

    ingest stream from binance ws queue

---
+++ polymarket/src/db/ingest_stream.py
@@ -0,0 +1,43 @@
+import asyncio
+import time
+from questdb.ingress import Sender, IngressError, TimestampNanos
+import sys
+import datetime
+
+
+def parse_message(message):
+    data = message[1]
+
+    payload = {
+        "symbol": data['s'],
+        "open": float(data['o']),
+        "volume": float(data['v']),
+        "timestamp_binance": data['C']
+    }
+    print(payload)
+    return payload
+
+
+
+async def insert_into_db(conf, table_name, queue, func, executor):
+    print("Starting DB insert task...")
+    with Sender.from_conf(conf) as sender:
+        while True:
+            message = await queue.get()  # Await queue
+            if message is None:          # Sentinel to stop
+                break
+
+            loop = asyncio.get_running_loop()
+            result = await loop.run_in_executor(executor, func, message)
+
+            at = datetime.datetime.fromtimestamp(time.time(), datetime.timezone.utc)
+            try:
+                sender.row(table_name=table_name, columns=result, at=at)
+            except IngressError as e:
+                sys.stderr.write(f'Got error: {e}\n')
+
+            queue.task_done()  # Mark processed
+
+
+
+# insert_into_db(conf=conf, table_name=table_name, payload=payload)
