8fe84b59095838aafe36ab54ae273f84e754f0a3
VP <vaclav.pazderka@logex.com>
2026-01-18 18:06:17 +0100

    moved file

---
+++ polymarket_dashboard/src/polymarket_dashboard/src/db/ingest_stream.py
@@ -0,0 +1,30 @@
+import asyncio
+import time
+import sys
+import datetime
+from questdb.ingress import Sender, IngressError
+
+
+async def insert_into_db(conf, table_name, queue, func, executor, logger):
+    logger.info("Starting DB insert task...")
+    with Sender.from_conf(conf) as sender:
+        while True:
+            message = await queue.get()  # Await queue
+            logger.info(f"Received message: {message}")
+            if message is None:          # Sentinel to stop
+                break
+
+            loop = asyncio.get_running_loop()
+            result = await loop.run_in_executor(executor, func, message, logger)
+
+            at = datetime.datetime.fromtimestamp(time.time(), datetime.timezone.utc)
+            try:
+                sender.row(table_name=table_name, columns=result, at=at)
+            except IngressError as e:
+                sys.stderr.write(f'Got error: {e}\n')
+
+            queue.task_done()  # Mark processed
+
+
+
+# insert_into_db(conf=conf, table_name=table_name, payload=payload)
