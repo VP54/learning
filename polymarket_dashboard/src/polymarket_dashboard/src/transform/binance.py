f9e4b3d4dc3d6cd847203b2da70cc4e45af3c5d2
VP <vaclav.pazderka@logex.com>
2026-01-20 17:54:15 +0100

    new parsing logic

--- polymarket_dashboard/src/polymarket_dashboard/src/transform/binance.py
+++ polymarket_dashboard/src/polymarket_dashboard/src/transform/binance.py
@@ -1,17 +1,20 @@
 import logging
-from polymarket_dashboard.src.polymarket_dashboard.src.utils.message_handler import register
-from polymarket_dashboard.src.polymarket_dashboard.src.config.enum import Exchange
+from polymarket_dashboard.src.utils.message_handler import register
+from polymarket_dashboard.src.config.enum import Exchange
 
 
-@register(Exchange.Binance)
+@register(Exchange.Binance.value)
 def parse_binance_message(data: dict, logger: logging.Logger):
     logger.debug(f"Parsing message: {data}")
     payload = {
         "exchange": Exchange.Binance.value,
         "symbol": data['s'],
         "open": float(data['o']),
+        "high": float(data['h']),
+        "low": float(data['l']),
+        "close": float(data['c']),
         "volume": float(data['v']),
-        "timestamp_binance": data['C']
+        "timestamp_exchange": data['C'] * 1000
     }
     logger.debug(payload)
     return payload
