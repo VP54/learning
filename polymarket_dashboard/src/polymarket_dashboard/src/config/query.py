f5a7d404237a4241d4be5b26a2c235f4c1664bfa
VP <vaclav.pazderka@logex.com>
2026-01-20 17:49:47 +0100

    new tables query

--- polymarket_dashboard/src/polymarket_dashboard/src/config/query.py
+++ polymarket_dashboard/src/polymarket_dashboard/src/config/query.py
@@ -11,11 +11,33 @@ SCHEMA_TRADES = """
     close DOUBLE,
     volume DOUBLE
 """
+INDEX_TRADES = "exchange"
+PARTITION_BY = "DAY"
 
-INDEX_TRADES = """
-    exchange
-"""
 
-PARTITION_BY = """
-    DAY 
+SCHEMA_BINANCE_TRADES = """
+    timestamp TIMESTAMP,
+    timestamp_exchange TIMESTAMP,
+    exchange SYMBOL,
+    ticker SYMBOL,
+    open DOUBLE,
+    high DOUBLE,
+    low DOUBLE,
+    close DOUBLE,
+    volume DOUBLE
 """
+INDEX_BINANCE_TRADES = "exchange"
+PARTITION_BINANCE_BY = "DAY"
+
+
+SCHEMA_POLYMARKET_TRADES = """
+    timestamp TIMESTAMP,
+    timestamp_exchange TIMESTAMP,
+    exchange SYMBOL,
+    bids DOUBLE[][],
+    asks DOUBLE[][],
+    ticker SYMBOL,
+    price DOUBLE
+"""
+INDEX_POLYMARKET_TRADES = "exchange"
+PARTITION_POLYMARKET_BY = "DAY"
