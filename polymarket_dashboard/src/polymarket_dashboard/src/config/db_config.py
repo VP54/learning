397335527113f8ff224663997e6f1bceaf5a7043
VP <vaclav.pazderka@logex.com>
2026-02-28 10:46:40 +0100

    added connection string method, some default param search

--- polymarket_dashboard/src/polymarket_dashboard/src/config/db_config.py
+++ polymarket_dashboard/src/polymarket_dashboard/src/config/db_config.py
@@ -3,8 +3,18 @@ import os
 
 
 class DatabaseConfig(BaseModel):
-    database: str
-    host: str = Field(default_factory=lambda: os.getenv("DB_HOST"))
-    port: int = Field(default_factory=lambda: int(os.getenv("DB_PORT")))
-    username: str = Field(default_factory=lambda: os.getenv("DB_USERNAME"))
-    password: str = Field(default_factory=lambda: os.getenv("DB_PASSWORD"))
+    host: str = Field(default_factory=lambda: os.getenv("DB_HOST",  "localhost"))
+    port: int = Field(default_factory=lambda: int(os.getenv("DB_PORT", 9000)))
+    username: str = Field(default_factory=lambda: os.getenv("DB_USERNAME", "admin"))
+    password: str = Field(default_factory=lambda: os.getenv("DB_PASSWORD", "quest"))
+    auto_flush_rows: int = Field(default_factory=lambda: int(os.getenv("DB_AUTO_FLUSH_ROWS", 100)))
+    auto_flush_interval: int = Field(default_factory=lambda: int(os.getenv("DB_AUTO_FLUSH_INTERVAL", 1000)))
+
+
+    def get_connection_string(self) -> str:
+        return (
+            f"http::addr={self.host}:{self.port};"
+            f"username={self.username};password={self.password};"
+            f"auto_flush_rows={self.auto_flush_rows};"
+            f"auto_flush_interval={self.auto_flush_interval};"
+        )
